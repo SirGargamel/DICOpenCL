67ef17579a79e267537bc225ca2f6e26a3628085
Petr Ječmen <petr.jecmen@tul.cz>
2013-11-27 14:49:47 +0100

    Test data with desciptions and data visualization.

--- .results/dataPlotLWScompare.m
+++ .results/dataPlotLWScompare.m
@@ -1,9 +1,10 @@
 clear all;
-% import csv data from DIC_OpenCL testing
-% Data format - VARIANT, IMAGE_WIDTH [px], IMAGE_HEIGHT [px], FACET_SIZE [px], DEFORMATION_COUNT ,LWS0 ,LWS1 ,Time [ms]
+% 2D kernel LWS comparison
+% Data format - VARIANT, IMAGE_WIDTH [px], IMAGE_HEIGHT [px], FACET_SIZE
+% [px], DEFORMATION_COUNT ,LWS0 ,LWS1 ,Total Time [ms], Kernel time [ms]
 
-counts = csvread('d:\\testData.csv',0,0,[0,0,0,1]);
-fullData = csvread('d:\\testData.csv',2,0);
+counts = csvread('Compute2DIntGpuDirect.csv',0,0,[0,0,0,1]);
+fullData = csvread('Compute2DIntGpuDirect.csv',2,0);
 % data preparation
 curves = zeros(counts(1), counts(2)) ;
 for curve=1:counts(1)
