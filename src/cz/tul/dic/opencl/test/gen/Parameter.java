ed82fe16cc4015eb27600f36630f83b043c9c46d
Petr Ječmen <petr.jecmen@tul.cz>
2013-12-11 10:10:41 +0100

    Parameter reordering.

--- src/cz/tul/dic/opencl/test/gen/Parameter.java
+++ src/cz/tul/dic/opencl/test/gen/Parameter.java
@@ -5,15 +5,15 @@ package cz.tul.dic.opencl.test.gen;
  * @author Petr Jecmen
  */
 public enum Parameter {
-
-    VARIANT,
+    
     IMAGE_WIDTH,
     IMAGE_HEIGHT,
     FACET_SIZE,
     FACET_COUNT,
     DEFORMATION_COUNT,
+    DATASIZE,
+    VARIANT,
     LWS0,
-    LWS1,    
-    DATASIZE
+    LWS1,        
 
 }
