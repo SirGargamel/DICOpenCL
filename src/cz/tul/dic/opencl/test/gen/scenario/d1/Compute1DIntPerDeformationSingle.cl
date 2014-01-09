83310327397a11f7f56631ac9f259175726bcb00
Petr Ječmen <petr.jecmen@tul.cz>
2014-01-09 12:40:42 +0100

    1D kernel for per deformation computation.

---
+++ src/cz/tul/dic/opencl/test/gen/scenario/d1/Compute1DIntPerDeformationSingle.cl
@@ -0,0 +1,101 @@
+int computeIndex(const float x, const float y, const int width) {
+    return (int)((y * width) + x);
+}
+
+int interpolate(const float x, const float y, global read_only int * image, const int imageWidth) {
+    const float ix = floor(x);
+    const float dx = x - ix;
+    
+    const float iy = floor(y);
+    const float dy = y - iy;
+
+    float intensity = 0;    
+    intensity += image[computeIndex(ix, iy, imageWidth)] * (1 - dx) * (1 - dy);
+    intensity += image[computeIndex(ix+1, iy, imageWidth)] * dx * (1 - dy);
+    intensity += image[computeIndex(ix, iy+1, imageWidth)] * (1 - dx) * dy;
+    intensity += image[computeIndex(ix+1, iy+1, imageWidth)] * dx * dy;
+
+    return intensity;    
+}
+/**
+ * @author Petr Jecmen
+ */
+kernel void Compute1DIntPerDeformationSingle(
+    global read_only int * imageA, global read_only int * imageB, 
+    global read_only int * facets, global read_only int * facetCenters,
+    global read_only float * deformation,
+    global write_only float * result,    
+    const int imageWidth, const int facetCount,
+    const int facetSize) 
+{
+    // id checks       
+    const size_t facetId = get_global_id(0);
+    if (facetId >= facetCount) {
+        return;
+    }
+    
+    // index computation
+    const int facetSize2 = facetSize * facetSize;
+    const int facetCoordCount = facetSize2 * 2;   
+    const int baseIndexFacet = facetId * facetCoordCount;         
+    const int baseIndexFacetCenter = facetId * 2;
+    // deform facet
+    float deformedFacet[50*50*2];    
+    int indexFacet, i2, x, y, dx, dy;   
+    for (int i = 0; i < facetSize2; i++) {
+        i2 = i*2;
+        indexFacet = baseIndexFacet + i2; 
+        
+        x = facets[indexFacet];
+        y = facets[indexFacet+1];
+
+        dx = x - facetCenters[baseIndexFacetCenter];
+        dy = y - facetCenters[baseIndexFacetCenter + 1];
+        
+        deformedFacet[i2] = x + deformation[0] + deformation[2] * dx + deformation[4] * dy;                    
+        deformedFacet[i2 + 1] = y + deformation[1] + deformation[3] * dx + deformation[5] * dy;    
+    }
+    // compute correlation using ZNCC
+    float deformedI[50*50];
+    float facetI[50*50];
+    float meanF = 0;
+    float meanG = 0; 
+    float val;
+    for (int i = 0; i < facetSize2; i++) {
+        i2 = i*2;
+        indexFacet = baseIndexFacet + i2;
+                
+        // facet is just array of int coords        
+        val = imageA[computeIndex(facets[indexFacet], facets[indexFacet + 1], imageWidth)];        
+        facetI[i] = val;
+        meanF += val;
+        
+        val = interpolate(deformedFacet[i2], deformedFacet[i2 + 1], imageB, imageWidth);                        
+        deformedI[i] = val;
+        meanG += val;
+    } 
+    meanF /= (float) facetSize2;
+    meanG /= (float) facetSize2;
+    
+    float deltaF = 0;
+    float deltaG = 0;    
+    for (int i = 0; i < facetSize2; i++) {                                     
+        val = facetI[i] - meanF;
+        facetI[i] = val;
+        deltaF += val * val;
+                
+        val = deformedI[i] - meanG;
+        deformedI[i] = val;
+        deltaG += val * val;
+    }    
+    const float deltaFs = sqrt(deltaF);
+    const float deltaGs = sqrt(deltaG);    
+    
+    float resultVal = 0;                  
+    for (int i = 0; i < facetSize2; i++) {                    
+        resultVal += facetI[i] * deformedI[i];
+    }    
+    
+    //store result    
+    result[facetId] = resultVal / (deltaFs * deltaGs);    
+}
---
+++ src/cz/tul/dic/opencl/test/gen/scenario/d1/Compute1DIntPerDeformationSingle.java
@@ -0,0 +1,118 @@
+package cz.tul.dic.opencl.test.gen.scenario.d1;
+
+import com.jogamp.opencl.CLBuffer;
+import com.jogamp.opencl.CLCommandQueue;
+import com.jogamp.opencl.CLContext;
+import com.jogamp.opencl.CLEvent;
+import com.jogamp.opencl.CLEventList;
+import com.jogamp.opencl.CLException;
+import com.jogamp.opencl.CLKernel;
+import static com.jogamp.opencl.CLMemory.Mem.READ_ONLY;
+import static com.jogamp.opencl.CLMemory.Mem.WRITE_ONLY;
+import cz.tul.dic.opencl.test.gen.ContextHandler;
+import cz.tul.dic.opencl.test.gen.Parameter;
+import cz.tul.dic.opencl.test.gen.ParameterSet;
+import cz.tul.dic.opencl.test.gen.Utils;
+import cz.tul.dic.opencl.test.gen.scenario.ScenarioResult;
+import java.io.IOException;
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+
+/**
+ *
+ * @author Petr Jecmen
+ */
+public class Compute1DIntPerDeformationSingle extends Scenario1D {
+
+    private static final String KERNEL_NAME = "Compute1DIntPerDeformationSingle";
+    
+    public Compute1DIntPerDeformationSingle(final ContextHandler contextHandler) throws IOException {
+        super(KERNEL_NAME, contextHandler);
+    }
+
+    @Override
+    ScenarioResult computeScenario(int[] imageA, int[] imageB, int[] facetData, int[] facetCenters, float[] deformations, ParameterSet params) {
+        final int facetSize = params.getValue(Parameter.FACET_SIZE);
+        final int facetCount = params.getValue(Parameter.FACET_COUNT);
+        final int deformationCount = deformations.length / Utils.DEFORMATION_DIM;
+        final float[] result = new float[facetCount * deformationCount];
+        // prepare buffers
+        final CLContext context = contextHandler.getContext();
+        final CLBuffer<IntBuffer> bufferImageA = context.createIntBuffer(imageA.length, READ_ONLY);
+        final CLBuffer<IntBuffer> bufferImageB = context.createIntBuffer(imageB.length, READ_ONLY);
+        final CLBuffer<IntBuffer> bufferFacetData = context.createIntBuffer(facetData.length, READ_ONLY);
+        final CLBuffer<IntBuffer> bufferFacetCenters = context.createIntBuffer(facetCenters.length, READ_ONLY);
+        final CLBuffer<FloatBuffer> bufferDeformation = context.createFloatBuffer(Utils.DEFORMATION_DIM, READ_ONLY);
+        final CLBuffer<FloatBuffer> bufferResult = context.createFloatBuffer(facetCount, WRITE_ONLY);
+        long clSize = bufferImageA.getCLSize() + bufferImageB.getCLSize() + bufferFacetData.getCLSize() + bufferDeformation.getCLSize() + bufferResult.getCLSize();
+        params.addParameter(Parameter.DATASIZE, (int) (clSize / 1000));
+        // fill constant buffers
+        fillBuffer(bufferImageA.getBuffer(), imageA);
+        fillBuffer(bufferImageB.getBuffer(), imageB);
+        fillBuffer(bufferFacetData.getBuffer(), facetData);
+        fillBuffer(bufferFacetCenters.getBuffer(), facetCenters);        
+        // prepare kernel arguments
+        final CLKernel kernel = contextHandler.getKernel();
+        kernel.putArgs(bufferImageA, bufferImageB, bufferFacetData, bufferFacetCenters, bufferDeformation, bufferResult)
+                .putArg(params.getValue(Parameter.IMAGE_WIDTH))
+                .putArg(facetCount)
+                .putArg(facetSize)
+                .rewind();
+        // prepare work sizes
+        final int lws0 = getLWS0();
+        final int facetGlobalWorkSize = roundUp(lws0, facetCount);
+        params.addParameter(Parameter.LWS0, lws0);
+        // execute kernel        
+        long duration = 0;
+        float[] oneResult;
+        try {
+            CLEventList eventList = new CLEventList(deformationCount);
+
+            final CLCommandQueue queue = contextHandler.getDevice().createCommandQueue(CLCommandQueue.Mode.PROFILING_MODE);
+
+            queue.putWriteBuffer(bufferImageA, false);
+            queue.putWriteBuffer(bufferImageB, false);
+            queue.putWriteBuffer(bufferFacetData, false);
+            queue.putWriteBuffer(bufferFacetCenters, false);
+
+            // obalit forem
+            for (int i = 0; i < deformationCount; i++) {
+                // plnit buffer spravnou deformaci
+                fillBuffer(bufferDeformation.getBuffer(), deformations, i * Utils.DEFORMATION_DIM, Utils.DEFORMATION_DIM);
+                // write new data and run kernel
+                queue.putWriteBuffer(bufferDeformation, false);
+
+                queue.put1DRangeKernel(kernel, 0, facetGlobalWorkSize, lws0, eventList);
+                queue.putReadBuffer(bufferResult, true);
+                oneResult = readBuffer(bufferResult.getBuffer());
+                // store result                
+                for (int r = 0; r < facetCount; r++) {
+                    result[(r * deformationCount) + i] = oneResult[r];
+                }
+
+                duration += eventList.getEvent(i).getProfilingInfo(CLEvent.ProfilingCommand.END) - eventList.getEvent(i).getProfilingInfo(CLEvent.ProfilingCommand.START);
+            }
+
+            // data cleanup
+            bufferImageA.release();
+            bufferImageB.release();
+            bufferFacetData.release();
+            bufferFacetCenters.release();
+            bufferDeformation.release();
+            bufferResult.release();
+            eventList.release();
+        } catch (CLException ex) {
+            System.err.println("CL error - " + ex.getLocalizedMessage());
+        }
+
+        return new ScenarioResult(result, duration);
+    }
+
+    private static void fillBuffer(FloatBuffer buffer, float[] data, int offset, int length) {
+        buffer.clear();
+        for (int i = offset; i < offset + length; i++) {
+            buffer.put(data[i]);
+        }
+        buffer.rewind();
+    }
+}
