97b4b349ca4c944aa56e6185d02bb2d3b8be16bd
Petr Ječmen <petr.jecmen@tul.cz>
2014-01-10 10:15:08 +0100

    Added possibility to choose which time will be used as data.

--- .results/LoadData.m
+++ .results/LoadData.m
@@ -6,7 +6,7 @@ pointCounts = csvread(FILE_NAME,0,3,[0,3,0,1+ variantCount + 1]);
 pointCount = sum(pointCounts) * testCaseCount;
 fid = fopen(FILE_NAME);
 allData = textscan(fid, FILE_FORMAT, 'headerlines', FILE_HEADER_LINE_COUNT, 'Delimiter', FILE_DELIMITER);
-data = cell2mat(allData(:, 1:INDEX_TIME));
+data = cell2mat(allData(:, 1:INDEX_TIME_KERNEL));
 % Lines extraction
 allCurves = NaN(COUNT_LWS1, COUNT_LWS0, variantCount, testCaseCount, graphCount);
 for graph=1:graphCount
@@ -31,6 +31,6 @@ for graph=1:graphCount
         % multiple variants, multiple test cases
         % one graph per data combination (w, h, fs, dc)
         % one line for each LWS0, variant and test case
-        allCurves(log2(lws1) + 1, log2(lws0) + 1, variant, testCase, graph) = data(index, INDEX_TIME);
+        allCurves(log2(lws1) + 1, log2(lws0) + 1, variant, testCase, graph) = data(index, ANALYZED_TIME);
     end;
 end;
