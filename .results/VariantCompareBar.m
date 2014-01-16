9cbbff4daeb494605b93544f18c1432fd6c3180a
Petr Ječmen <petr.jecmen@tul.cz>
2014-01-16 13:40:42 +0100

    Convert to stacked bar plot to show how much time was spent copying data.

--- .results/VariantCompareBar.m
+++ .results/VariantCompareBar.m
@@ -6,16 +6,17 @@ clc;
 Constants;
 LoadData;
 INSPECTED_TEST_CASE = TEST_CASE_SHIFT;
+ANALYZED_TIME = TIME_TOTAL;
 % Find best curves (fastest) for each variant
-bestCurves = NaN(variantCount, graphCount);
-bestCurvesParams = NaN(2, variantCount, graphCount);
+bestCurves = NaN(2,variantCount, graphCount);
 for graph=1:graphCount
     for var=1:variantCount
-        m = allCurves(ANALYZED_TIME,:,:,var,INSPECTED_TEST_CASE,graph);
-        [minVal, index] = min(m(:));
-        [minLws1, minLws0] = ind2sub(size(m(:)), index);
+        m = squeeze(allCurves(ANALYZED_TIME,:,:,var,INSPECTED_TEST_CASE,graph));        
+        [minVal, index] = min(m(:));        
+        [minLws1, minLws0] = ind2sub(size(m), index);
 
-        bestCurves(var, graph) = minVal;        
+        bestCurves(1, var, graph) = allCurves(TIME_KERNEL,minLws1,minLws0,var,INSPECTED_TEST_CASE,graph);
+        bestCurves(2, var, graph) = allCurves(TIME_TOTAL,minLws1,minLws0,var,INSPECTED_TEST_CASE,graph) - bestCurves(1, var, graph);
     end;
 end;
 % Plot graphs
@@ -39,14 +40,14 @@ for win=1:windowCount
             end;   
             index = ((innerBase - 1) * pointCount) + 1;
             % create subplot
-            h = subplot(graphCountX, graphCountY, (graphY-1) * graphCountX + graphX);                                 
+            subplot(graphCountY, graphCountX, (graphY-1) * graphCountX + graphX);
             ylabel('Time [ms]');                        
             title(cellstr([int2str(data(index, INDEX_RESX)) 'x' int2str(data(index, INDEX_RESY)) ', fs=' int2str(data(index, INDEX_FACET_SIZE)) ', dc=' int2str(data(index, INDEX_DEFORMATION_COUNT))]));
             % plot both curves to one subfigure       
             hold on;                                    
-            bar(bestCurves(:, innerBase));
-            set(gca, 'XTickLabel',NAMES_VARIANTS);
-
+            bar(bestCurves(:, :, innerBase)','stacked');
+            set(gca,'XTick', X(1:numel(bestCurves(1, :, innerBase))), 'XTickLabel',NAMES_VARIANTS);
+            fix_xticklabels();
             % Finish plotting to subfigure
             hold off;
         end;
