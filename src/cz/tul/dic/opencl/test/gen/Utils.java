3fa861e1a7cce15fafbbec3fdc273225ed73887a
Petr Ječmen <petr.jecmen@tul.cz>
2014-02-07 13:45:52 +0100

    Adjust precision.

--- src/cz/tul/dic/opencl/test/gen/Utils.java
+++ src/cz/tul/dic/opencl/test/gen/Utils.java
@@ -7,8 +7,8 @@ package cz.tul.dic.opencl.test.gen;
 public abstract class Utils {
 
     public static final int DEFORMATION_DIM = 6;
-    public static final float EPS_NORMAL = 0.05f;
-    public static final float EPS_PRECISE = 0.001f;
+    public static final float EPS_NORMAL = 0.15f;
+    public static final float EPS_PRECISE = 0.01f;
     public static final float DEFORMATION_ABS_MAX_0 = 5;
     public static final float DEFORMATION_ABS_MAX_1 = 0.1f; 
 
