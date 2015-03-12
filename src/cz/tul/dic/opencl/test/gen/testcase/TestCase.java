d32d9ff72d93ecf5c97f50b1437fe08df00972d0
Petr Ječmen <petr.jecmen@tul.cz>
2015-03-12 13:03:53 +0100

    Revert offset change.

--- src/cz/tul/dic/opencl/test/gen/testcase/TestCase.java
+++ src/cz/tul/dic/opencl/test/gen/testcase/TestCase.java
@@ -33,7 +33,7 @@ public class TestCase {
         Random rnd = new Random();
         int centerX, centerY, baseIndex;
         final double s_2 = Math.ceil(size / 2.0);
-        final int offset = (int) (2*s_2 + Utils.DEFORMATION_ABS_MAX_0 + (2 * Utils.DEFORMATION_ABS_MAX_1 * s_2));
+        final int offset = (int) (s_2 + Utils.DEFORMATION_ABS_MAX_0 + (2 * Utils.DEFORMATION_ABS_MAX_1 * s_2));
         for (int i = 0; i < count; i++) {
             baseIndex = i * 2;
             centerX = rnd.nextInt(width - (2 * offset)) + offset;
